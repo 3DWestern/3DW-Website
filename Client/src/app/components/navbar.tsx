845068f97ef913b93e25f51b1b1ceef9312a4b1f
Thomson-Lam <thomson.lam327@gmail.com>
2025-02-10 14:22:43 -0500

    added basic navigation

---
+++ Client/src/app/components/navbar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function Navbar() { // using list items and unordered list for better accessiblity
+    return (
+        <nav className="bg-gray-800 p-4">
+            <ul className="flex space-x-4 text-white">
+                <li>
+                    <Link href="/">
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/activities">
+                        Activities
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/about">
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/contact">
+                        Contact
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/gallery">
+                        Gallery
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
