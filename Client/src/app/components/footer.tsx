851d7c1d484770e6ff156b00ff6c89c8a419fd22
Thomson-Lam <thomson.lam327@gmail.com>
2025-03-11 14:14:34 -0400

    added basic footer component

--- Client/src/app/components/footer.tsx
+++ Client/src/app/components/footer.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import FooterButton from './footer-button';
 
-// placeholder footer component for the pages 
+// TODO: responsiveness font + flex shrinkable footer icons and input  
 export default function Footer () {
     return(
-        <h1 className="">footer component</h1>
+        <div className="bg-black/70 opacity-80 text-white gap-y-1">
+            <div className="flex items-center justify-center bg-red-700">
+                <h1>SUBSCRIBE</h1>
+            </div>
+            <div className="bg-blue-50">
+                <h1>Input</h1>
+                <FooterButton /> 
+            </div>
+            <div className="bg-red-70">
+                <h1>icons</h1>
+            </div>
+            <div className="bg-white">
+                <h1>signature</h1>
+            </div>
+        </div>
     );
 }
