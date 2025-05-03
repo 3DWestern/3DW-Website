1ce3ca4722848ce7fe7d4ed8de487da1c9a38f54
Thomson-Lam <thomson.lam327@gmail.com>
2025-05-02 19:25:39 -0800

    added contact page; TODO: reformat global layout and add custom layout

---
+++ Client/src/app/about/teamdata.tsx
@@ -0,0 +1,70 @@
+export type Exec = {
+    name: string;
+    title: string;
+    src: string;
+    alt: string;
+    url: string;
+    bgColor: string;
+}
+
+const teamdata: Exec[] = [
+    {
+        name: "Troy Leishman",
+        title: "President",
+        src: "/troy.webp",
+        alt: "Troy, President of 3DW",
+        url: "linkedin.ca",
+        bgColor: "bg-red-500"
+    },
+    {
+        name: "Emma Zhang",
+        title: "VP Events",
+        src: "/emma.webp",
+        alt: "Emma, VP Events",
+        url: "linkedin.ca",
+        bgColor: "bg-teal-500"
+    },
+    {
+        name: "Kevin Shang",
+        title: "VP Finance",
+        src: "/kevin.webp",
+        alt: "Kevin, VP Finance",
+        url: "linkedin.ca",
+        bgColor: "bg-purple-500",
+    },
+    {
+        name: "Doris Wang",
+        title: "Communications",
+        src: "/doris.webp",
+        alt: "Doris Wang, Communications",
+        url: "linkedin.ca",
+        bgColor: "bg-teal-500",
+    },
+    {
+        name: "Johaan Khan",
+        title: "VP Educations",
+        src: "/johaan.webp",
+        alt: "Johaan Khan, VP Educations",
+        url: "linkedin.ca",
+        bgColor: "bg-purple-500",
+    },
+    {
+        name: "Sabrina Luo",
+        title: "VP Marketing",
+        src: "/sabrina.webp",
+        alt: "Sabrina Luo, VP Marketing",
+        url: "linkedin.ca",
+        bgColor: "bg-teal-500",
+    },
+    {
+        name: "Kyler See",
+        title: "VP Outreach",
+        src: "/kyler.webp",
+        alt: "Kyler See, VP Outreach",
+        url: "linkedin.ca",
+        bgColor: "bg-purple-500", 
+    }
+];
+
+
+export default teamdata; 
