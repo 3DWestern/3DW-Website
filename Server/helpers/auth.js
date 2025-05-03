4347293289f79edce3cec724d74ef1a92d7c7f08
Josh Muszka <jmuszka@uwo.ca>
2025-05-03 15:17:40 -0400

    feat: handle cases where student id already exists in database

--- Server/helpers/auth.js
+++ Server/helpers/auth.js
@@ -1,10 +1,12 @@
 //const pool = require('../db.js');
-//const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const bcrypt = require("bcrypt");
 const { MongoClient } = require("mongodb");
 
 const client = new MongoClient(process.env.ATLAS_URI);
 const database = client.db(process.env.DB);
+
+const NUM_OF_SALT_ROUNDS = 12; // more salting ==> more secure
 
 //Query the database for the credentials
 //Return as a boolean
@@ -22,31 +24,33 @@ const queryDBCredentials = async (username, password) => {
     return await bcrypt.compare(password, result.password_hash)
 }
 
-//Add the credentials to the database
+// Add the credentials to the database
+// Returns true if user successfully added, returns false if user with student id already exists
+// Throws error if anything else happens
 const addDBCredentials = async (studentNumber, email, password, firstName, lastName) => {
 
-    // First hash the password, then add to database
-    bcrypt.hash(password, 12, (err, hash) => { 
-
-        // Connect to 'users' cluster
-        const users = database.collection('users');
-
-        // Create object for new user, according to database schema
-        const newUser = {
-            _id: { student_id: studentNumber },
-            email: email,
-            name: {
-                first: firstName,
-                last: lastName
-            },
-            password_hash: hash,
-            account_created: new Date().toISOString(),
-            membership: "MEMBER" // TODO: are we going to allow non-members to sign up?
-        }
-
-        // Add new user to database
-        users.insertOne(newUser)
-    })
+    // Hash the password
+    let hash = await bcrypt.hash(password, NUM_OF_SALT_ROUNDS)
+    
+    // Connect to cluster and prepare information
+    const users = database.collection('users');
+    const newUser = {
+        _id: { student_id: studentNumber },
+        email: email,
+        name: { first: firstName, last: lastName},
+        password_hash: hash,
+        account_created: new Date().toISOString(),
+        membership: "MEMBER" // TODO: are we going to allow non-members to sign up?
+    }
+    
+    // Add to database
+    try {
+        await users.insertOne(newUser)
+        return true;
+    } catch (error) {
+        if (error.code === 11000) return false // this means user already exists
+        else throw new Error(error);
+    }
 }
 
 const generateRefreshToken = (username) => {
