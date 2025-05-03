d6c91ad8e44926e576baf60e8284d644f3d1391a
Thomson-Lam <thomson.lam327@gmail.com>
2025-05-03 13:29:09 -0800

    edited layout for pages

---
+++ Client/src/app/about/layout.tsx
@@ -0,0 +1,12 @@
+import Footer from '../components/footer';
+
+export default function AboutLayout ({children} : {children: React.ReactNode}) {
+   return(
+    <div>
+        <main className="flex flex-col min-h-screen bg-gradient-to-l from-fuchsia-800 via-fuchsia-800 to-purple-900">
+            {children}
+        </main>
+        <Footer />
+    </div>
+    );
+}
---
+++ Client/src/app/contact/layout.tsx
@@ -0,0 +1,12 @@
+import Footer from '../components/footer';
+
+export default function ContactLayout ({children} : {children: React.ReactNode}) {
+   return(
+    <div>
+        <main className="flex flex-col min-h-screen bg-black">
+            {children}
+        </main>
+        <Footer />
+    </div>
+    );
+}
