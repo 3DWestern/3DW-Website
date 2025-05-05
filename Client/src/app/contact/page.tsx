c2b35f97fb72efc84b05d5a49d02842fafa60d3a
Thomson Lam <145819823+Thomson-Lam@users.noreply.github.com>
2025-05-04 20:13:00 -0400

    Update page.tsx

--- Client/src/app/contact/page.tsx
+++ Client/src/app/contact/page.tsx
@@ -3,8 +3,7 @@ import ContactForm from '../components/contactform';
 
 export default function Contact () {
     return (
-       <div className="bg-red-700">
-       </div> 
+      <ContactForm /> 
     );
 
 }
