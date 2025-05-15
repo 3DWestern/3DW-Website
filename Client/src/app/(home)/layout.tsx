d248ff36e86c6d9d7e7d25af2028b9ede6c62b41
Thomson-Lam <thomson.lam327@gmail.com>
2025-05-15 12:03:49 -0800

    completed basic layout for landing page

--- Client/src/app/(home)/layout.tsx
+++ Client/src/app/(home)/layout.tsx
@@ -1,10 +1,14 @@
 import Navbar from '../components/navbar';
+//import Footer from '../components/footer';
+//     <div className="transition-colors bg-gradient-to-l from-fuchsia-800 via-fuchsia-800 to-purple-900">
 
 export default function LoginLayout ({children} : {children: React.ReactNode}) {
    return(
-    <div className="transition-colors bg-gradient-to-l from-fuchsia-800 via-fuchsia-800 to-purple-900">
+    <div className="flex flex-col transition-colors bg-gradient-to-l from-fuchsia-800 via-fuchsia-800 to-purple-900">
+        <div className="fixed top-0 left-0 w-full z-50">
         <Navbar />
-        <main className="flex flex-col">
+        </div>
+        <main>
             {children}
         </main>
     </div>
--- Client/src/app/(home)/page.tsx
+++ Client/src/app/(home)/page.tsx
@@ -1,14 +1,22 @@
-import Images from '../components/images';
-import Landing from '../components/landing';
-import images from './images'; 
+'use client';
+// import Images from '../components/images';
+// import Landing from '../components/landing';
+//import images from './images'; 
 import Footer from '../components/footer';
-// sections 
 
-// TODO: place footer outside of div, place footer in scrollable and see how it goes 
-
+// TODO: Activites, async server comps, footer adjust size,, text overlay, icons for contact, landing page components.  
 export default function Home() {
   return (
-  <div className="h-screen w-full overflow-y-scroll scroll-smooth snap-y snap-mandatory">
+    <div className="h-screen overflow-y-scroll scroll-smooth snap-y snap-mandatory hide-scrollbar">
+  {/** TOP SECTION (parallax + snap) */}
+  <section
+    className="h-screen snap-start bg-black bg-cover bg-center bg-fixed flex items-center justify-center"
+    style={{ backgroundImage: "url('/man.jpeg')" }}
+  >
+    {/* Optional content in center */}
+  </section>
+
+  {/** BOTTOM SECTIONS */}
   <section className="h-screen snap-start bg-red-200 flex items-center justify-center">
     <h1>Section 1</h1>
   </section>
@@ -17,13 +25,15 @@ export default function Home() {
     <h1>Section 2</h1>
   </section>
 
-  <section className="h-screen snap-start bg-green-200 flex items-center justify-center mb-2">
+  <section className="h-screen snap-start bg-green-200 flex items-center justify-center">
     <h1>Section 3</h1>
   </section>
-  
-  <div className="h-1/2 snap-start flex items-center justify-center w-full">
-  <Footer />
-  </div>
+
+  <section className="h-1/3 bg-red-700 snap-start flex items-center justify-center w-full">
+    <Footer />
+  </section>
 </div>
+
   );
+  
 }
