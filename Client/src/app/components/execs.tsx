5ca9f01eb8b41f92d7f8d12d7f4b068ac411088d
Josh Muszka <josh@jmuszka.com>
2025-06-28 09:10:02 -0400

    feat: default to blank pfps and remove <a> links if linkedin url is undefined

--- Client/src/app/components/execs.tsx
+++ Client/src/app/components/execs.tsx
@@ -9,39 +9,53 @@ type Exec = { // duplicated this to avoid execssive importing down the component
     src: string;
     alt: string;
     url: string;
-    bgColor: string; 
+    bgColor: string;
 };
 
 
-export default function Execs ({execs} : {execs: Exec[]}) {
+export default function Execs({ execs }: { execs: Exec[] }) {
     return (
         <div className="w-2/3 flex flex-wrap flex col justify-center items-center mx-auto my-10">
             {execs.map((data: Exec, index) => (
                 <div key={index}>
-                   <Tilt 
-                   glareEnable={true}
-                   glareColor={data.bgColor}
-                   scale={1.5}
-                   transitionSpeed={600}
-                   tiltMaxAngleX={25}
-                   tiltMaxAngleY={25}
-                   className="flex-shrink-0 m-10 rounded-xl max-w-xs">
-                   <a className="flex flex-col items-center justify-center z-50" href={"https://" + data.url} target="_blank" rel="noopener noreferrer">
-                    <Image 
-                        width={180}
-                        height={320}
-                        alt={data.alt} 
-                        src={data.src}
-                        className="h-28 w-28 object-cover my-2 rounded-xl text-white"
-                        />
-                        <div className="text-center text-white my-3 p-1">
-                            <h1 className="block font-semibold text-2xl">{data.name}</h1>
-                            <h2 className="text-lg px-3 pt-1 text-center mt-1 leading-tight">{data.title}</h2>
-                        </div> 
-                   </a>
-                   </Tilt>
+                    <Tilt
+                        glareEnable={true}
+                        glareColor={data.bgColor}
+                        scale={1.5}
+                        transitionSpeed={600}
+                        tiltMaxAngleX={25}
+                        tiltMaxAngleY={25}
+                        className="flex-shrink-0 m-10 rounded-xl max-w-xs">
+                        {data.url ?
+                            <a className="flex flex-col items-center justify-center z-50" href={"https://" + data.url} target="_blank" rel="noopener noreferrer">
+                                <Image
+                                    width={180}
+                                    height={320}
+                                    alt={data.alt}
+                                    src={data.src || "/blank.webp"}
+                                    className="h-28 w-28 object-cover my-2 rounded-xl text-white"
+                                />
+                                <div className="text-center text-white my-3 p-1">
+                                    <h1 className="block font-semibold text-2xl">{data.name}</h1>
+                                    <h2 className="text-lg px-3 pt-1 text-center mt-1 leading-tight">{data.title}</h2>
+                                </div>
+                            </a> : <div className="flex flex-col items-center justify-center z-50">
+                                <Image
+                                    width={180}
+                                    height={320}
+                                    alt={data.alt}
+                                    src={data.src || "/blank.webp"}
+                                    className="h-28 w-28 object-cover my-2 rounded-xl text-white"
+                                />
+                                <div className="text-center text-white my-3 p-1">
+                                    <h1 className="block font-semibold text-2xl">{data.name}</h1>
+                                    <h2 className="text-lg px-3 pt-1 text-center mt-1 leading-tight">{data.title}</h2>
+                                </div>
+                            </div>
+                        }
+                    </Tilt>
                 </div>
-            ))} 
+            ))}
         </div>
     );
 }
