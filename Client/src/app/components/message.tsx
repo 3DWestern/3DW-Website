b6ce3be858492f4d7d4c57a3dbdd0dd58487082f
Thomson-Lam <thomson.lam327@gmail.com>
2025-06-30 15:50:21 -0500

    adjusted styling for login page message

--- Client/src/app/components/message.tsx
+++ Client/src/app/components/message.tsx
@@ -4,22 +4,22 @@ import ShineBorder from './shineborder';
 // A temporary message component for incoming changes. NOTE: Ignore the img optimization suggestion, since Image does not work on gifs.
 export default function Message ({ message, header } : { message: string, header: string }) {
     return (
-        <div className="relative overflow-hidden mx-auto my-10 bg-white rounded-lg w-1/2 h-[25rem] md:h-[20rem]">
+        <div className="overflow-hidden relative my-10 mx-auto w-1/2 bg-white rounded-lg h-[25rem] md:h-[20rem]">
             <ShineBorder shineColor={["#A07CFE", "#FE8FB5", "#FFBE7B"]} />
             <Image 
             src="/amongus.png" 
             alt="Amongus" 
             width={50}
             height={80}
-            className="z-[100] rotate-45" />
-        <div className="text-black text-center mx-auto my-auto flex flex-col justify-center items-center">
+            className="rotate-45 z-[100]" />
+        <div className="flex flex-col justify-center items-center my-auto mx-auto text-center text-black">
             <div className="my-2">
-            <h1 className="text-2xl md:text-3xl">{header}
-            <img src="/3d-printer-intensifies.gif" alt="3D printer gif" className="inline-block h-5 w-5 md:h-8 md:w-8 ml-2"/>
+            <h1 className="px-2 text-2xl sm:text-3xl md:text-5xl">{header}
+            <img src="/3d-printer-intensifies.gif" alt="3D printer gif" className="inline-block ml-2 w-5 h-5 md:w-8 md:h-8"/>
             </h1>
             </div>
-            <div className="mx-auto p-3">
-            <p className="text-sm md:text-lg sm:text-md">{message}</p>
+            <div className="p-3 mx-auto">
+            <p className="sm:text-lg md:text-xl text-md">{message}</p>
             </div>
         </div>
         </div>
--- Client/src/app/login/page.tsx
+++ Client/src/app/login/page.tsx
@@ -1,12 +1,12 @@
-//import LoginForm from '.././components/loginform';
 import Message from '.././components/message';
-
 
 export default function Login() { // login page for auth 
     return (
-        <div className="mt-10">
-{/*            <LoginForm /> */}
+        <div className="flex flex-col justify-center items-center h-screen">
 	<Message header="Print Dashboard Coming Soon!" message="We are working to get the print system ready. Thank you for your patience!" />
         </div>
     );
 }
+
+
+
