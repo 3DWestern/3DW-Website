e9cc0eaa47ea1f5dd16e548f0af6804b829a9942
Thomson-Lam <thomson.lam327@gmail.com>
2025-06-30 14:34:23 -0500

    added msg for login page

--- Client/src/app/login/page.tsx
+++ Client/src/app/login/page.tsx
@@ -1,10 +1,12 @@
-import LoginForm from '.././components/loginform';
+//import LoginForm from '.././components/loginform';
+import Message from '.././components/message';
+
 
 export default function Login() { // login page for auth 
     return (
         <div className="mt-10">
-            <span className="text-black dark:text-white text-4xl sm:text-5xl font-bold text-center absolute w-full">Ordering system coming soon!</span>
-            <LoginForm />
+{/*            <LoginForm /> */}
+	<Message header="Print Dashboard Coming Soon!" message="We are working to get the print system ready. Thank you for your patience!" />
         </div>
     );
 }
