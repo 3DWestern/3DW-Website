417ef5deacc8497745a2a103b05ffc374292bba1
Thomson-Lam <thomson.lam327@gmail.com>
2025-06-30 14:26:33 -0500

    enlarged exec cards

--- Client/src/app/components/execs.tsx
+++ Client/src/app/components/execs.tsx
@@ -15,7 +15,7 @@ type Exec = { // duplicated this to avoid execssive importing down the component
 
 export default function Execs({ execs }: { execs: Exec[] }) {
     return (
-        <div className="w-2/3 flex flex-wrap flex col justify-center items-center mx-auto my-10">
+        <div className="flex flex-wrap justify-center items-center my-10 mx-auto w-2/3 col">
             {execs.map((data: Exec, index) => (
                 <div key={index}>
                     <Tilt
@@ -25,31 +25,31 @@ export default function Execs({ execs }: { execs: Exec[] }) {
                         transitionSpeed={600}
                         tiltMaxAngleX={25}
                         tiltMaxAngleY={25}
-                        className="flex-shrink-0 m-10 rounded-xl max-w-xs">
+                        className="flex-shrink-0 m-10 rounded-xl">
                         {data.url ?
-                            <a className="flex flex-col items-center justify-center z-50" href={"https://" + data.url} target="_blank" rel="noopener noreferrer">
+                            <a className="flex z-50 flex-col justify-center items-center" href={"https://" + data.url} target="_blank" rel="noopener noreferrer">
                                 <Image
-                                    width={180}
-                                    height={320}
+                                    width={100}
+                                    height={100}
                                     alt={data.alt}
                                     src={data.src || "/blank.webp"}
-                                    className="h-28 w-28 object-cover my-2 rounded-xl text-white"
+                                    className="object-cover my-2 w-40 h-40 text-white rounded-xl"
                                 />
-                                <div className="text-center text-white my-3 p-1">
-                                    <h1 className="block font-semibold text-2xl">{data.name}</h1>
-                                    <h2 className="text-lg px-3 pt-1 text-center mt-1 leading-tight">{data.title}</h2>
+                                <div className="p-1 my-3 text-center text-white">
+                                    <h1 className="block text-3xl font-semibold">{data.name}</h1>
+                                    <h2 className="px-3 pt-1 mt-1 text-lg leading-tight text-center">{data.title}</h2>
                                 </div>
-                            </a> : <div className="flex flex-col items-center justify-center z-50">
+                            </a> : <div className="flex z-50 flex-col justify-center items-center">
                                 <Image
-                                    width={180}
+                                    width={240}
                                     height={320}
                                     alt={data.alt}
                                     src={data.src || "/blank.webp"}
-                                    className="h-28 w-28 object-cover my-2 rounded-xl text-white"
+                                    className="object-cover my-2 w-40 h-40 text-white rounded-xl"
                                 />
-                                <div className="text-center text-white my-3 p-1">
-                                    <h1 className="block font-semibold text-2xl">{data.name}</h1>
-                                    <h2 className="text-lg px-3 pt-1 text-center mt-1 leading-tight">{data.title}</h2>
+                                <div className="p-1 my-3 text-center text-white">
+                                    <h1 className="block text-2xl font-semibold">{data.name}</h1>
+                                    <h2 className="px-3 pt-1 mt-1 text-lg leading-tight text-center">{data.title}</h2>
                                 </div>
                             </div>
                         }
